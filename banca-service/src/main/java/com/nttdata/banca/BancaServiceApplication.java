d537400f59fb7d0e72edd6fc9fed8dea4a3736bb
neySistemas <neypam100@gmail.com>
2022-07-07 18:15:37 -0500

    banca_complete

--- banca-service/src/main/java/com/nttdata/banca/BancaServiceApplication.java
+++ banca-service/src/main/java/com/nttdata/banca/BancaServiceApplication.java
@@ -3,12 +3,12 @@ package com.nttdata.banca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class BancaServiceApplication {
 
